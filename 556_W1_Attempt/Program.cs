d3fd0d02effdf862c49dd69a1b0e0cf4f4996204
Molly <107332652+adjlfkld@users.noreply.github.com>
2024-05-01 23:32:14 +1000

    Week1_question4_attempt

--- 556_W1_Attempt/Program.cs
+++ 556_W1_Attempt/Program.cs
@@ -4,24 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        // prompt users for a number of miles
+        // prompt users enter a numeric high school grade point average
+        // promt users enter an admission test score
         // pass the value and converts the value to kilometers
         // A mile is 1.60934 kilometers
-        const double ONE_MILE = 1.60934;
+        double gpa;
+        int testScore;
+        const double MINGPA = 3.0;
+        const int ADMIN1 = 60, ADMIN2 = 80;
 
-        Console.WriteLine("Enter the number of miles:");
-        double miles =Convert.ToDouble(Console.ReadLine());
-        double kilometers;
-        kilometers= ConvertMilesToKilometers(miles, ONE_MILE);
-        Console.WriteLine("The kilometer is {0}", kilometers);
+        Console.WriteLine("Enter grade point average:");
+        gpa = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter grade point average:");
+        testScore = Convert.ToInt32(Console.ReadLine());
+
+        string CheckAdmission = AdmissionModularized(gpa, testScore, MINGPA, ADMIN1, ADMIN2);
+        Console.WriteLine("The Admission is {0}", CheckAdmission);
 
 
     }
 
-    private static double ConvertMilesToKilometers(double miles, double ONE_MILE) {
-        double kilometers = ONE_MILE * miles;
-        return kilometers;
-    }
+    private static string AdmissionModularized(double gpa, int testScore, double MINGPA, int ADMIN1, int ADMIN2) {
+
+        if (gpa >= MINGPA)
+        {
+            if (testScore >= ADMIN1)
+            {
+                return "Accept";
+
+            }
+            else
+            {
+                return "Reject";
+            }
+        }
+        else
+            if (testScore >= ADMIN2)
+            return "Accept";
+        else
+            return "Reject";
+    } 
 
 
 
